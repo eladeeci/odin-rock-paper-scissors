74db63551e5d8b0c694490a47081ed177caee23a
eladeeci <157269617+eladeeci@users.noreply.github.com>
2024-01-30 01:45:30 +0100

    Added the script function getPlayerChoice.
    Prompts the user, as Player 1, to input a hand to play among rock, paper, and scissors.

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-const choice = ['Rock', 'Paper', 'Scissors']
+const choice = ['rock', 'paper', 'scissors']
 
+function getPlayerChoice () {
+    playerChoice = prompt('Choose a hand to play: ','Rock, paper, or scissors.')
+    return playerChoice;
+}
 
 function getComputerChoice () {
     computerChoice = choice[Math.floor((Math.random()*choice.length))];
     return computerChoice;
 }
 
-console.log(getComputerChoice());
+
